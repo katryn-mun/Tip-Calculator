70cf1601f29acf10524fc2250babfca3f001b1d7
katryn-mun <162220060+katryn-mun@users.noreply.github.com>
2024-03-04 18:42:26 -0500

    reformatted pseudocode, removed lines

--- tipCalculator.js
+++ tipCalculator.js
@@ -1,10 +1,8 @@
-// Get whatever information is inputted into the box to be saved.
-// calculate the tip!!
-// find the element where the tip will go
-// insert the tip amount into the html
-// that's it
-
-
+// 1. Get whatever information is inputted into the box to be saved.
+// 2. calculate the tip!!
+// 3. find the element where the tip will go
+// 4. insert the tip amount into the html
+// 5. that's it
 
 //locate tip form 
 document.getElementById("tipForm").addEventListener('submit', function(event){
@@ -14,7 +12,6 @@ document.getElementById("tipForm").addEventListener('submit', function(event){
 
     console.log(billTotal)
 })
-
 
 // addEventListener('h', function(){ 
 //     //do stuff
