e85a3f978b4231cdc2c9e0c4bea23c73db28a496
katryn-mun <162220060+katryn-mun@users.noreply.github.com>
2024-03-05 21:15:44 -0500

    added functionality, changed html fiile name

--- tipCalculator.js
+++ tipCalculator.js
@@ -1,5 +1,5 @@
 // 1. Get whatever information is inputted into the box to be saved.
-// 2. calculate the tip!!
+// 2. calculate the tip!!    <------------------------
 // 3. find the element where the tip will go
 // 4. insert the tip amount into the html
 // 5. that's it
@@ -7,10 +7,20 @@
 //locate tip form 
 document.getElementById("tipForm").addEventListener('submit', function(event){
     event.preventDefault();
+    //CONSTANTS
+    const BILL_TOTAL = parseFloat(document.getElementById("bill-total").value)
+    const PERCENTAGE = 0.15
+    const TIP_ELEMENT= document.getElementById("tip")
+    const TIP = BILL_TOTAL * PERCENTAGE
+    const GRAND_TOTAL_ELEMENT = document.getElementById("grand-total")
+    const GRAND_TOTAL= BILL_TOTAL + TIP
 
-    let billTotal = document.getElementById("bill-total").value
+    //VARIABLES
+    let grandTotal= 0.00
 
-    console.log(billTotal)
+    TIP_ELEMENT.textContent = TIP.toFixed(2)
+    GRAND_TOTAL_ELEMENT.textContent=GRAND_TOTAL.toFixed(2)
+
 })
 
 // addEventListener('h', function(){ 
